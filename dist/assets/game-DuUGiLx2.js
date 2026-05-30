8742361630e13f6d836e0d5a7993f9c4415ed00f
Oleg Olanovski <oleg-olanovski@t-online.de>
2026-05-30 16:39:32 +0200

    add initial Vite, TypeScript and SCSS setup for memory game

---
+++ dist/assets/game-DuUGiLx2.js
@@ -0,0 +1,2 @@
+import{t as e}from"./main-Dm0c06KA.js";e();
+//# sourceMappingURL=game-DuUGiLx2.js.map
---
+++ dist/assets/main-DuUGiLx2.js
@@ -0,0 +1,2 @@
+import{t as e}from"./main-Dm0c06KA.js";e();
+//# sourceMappingURL=main-DuUGiLx2.js.map
