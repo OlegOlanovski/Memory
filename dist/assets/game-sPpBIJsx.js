e204b4a2d615ef87a7d7be0c9d57173900fb77e1
Oleg Olanovski <oleg-olanovski@t-online.de>
2026-06-04 09:19:44 +0200

    Refine semantic HTML structure for start and settings pages

--- dist/assets/game-sPpBIJsx.js
+++
@@ -1,2 +0,0 @@
-import{t as e}from"./main-nYIg_8sR.js";e();
-//# sourceMappingURL=game-sPpBIJsx.js.map
---
+++ dist/assets/settings-B_hUU6P8.js
@@ -0,0 +1,2 @@
+import{t as e}from"./main-C_oTa8QE.js";e();
+//# sourceMappingURL=settings-B_hUU6P8.js.map
